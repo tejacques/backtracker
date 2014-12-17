9bd93100cc0c5040f74932e518bf8fad817e7675
Tom Jacques <tejacques@gmail.com>
2014-12-16 20:45:36 -0500

    Nearly complete

--- src/backtracker.js
+++ src/backtracker.js
@@ -1,20 +1,64 @@
-(function () {
-    // Must have HTML5 history API
-    var history = window.history;
-    if (!history) {
-        return;
-    }
+(function (window, onback, onforward) {
 
-    // Set the state to have the length plus one
-    history.replaceState({historyLength: history.length + 1}, null, null);
+    var setup = function () {
+        console.log("I ran at all");
+        // Must have HTML5 history API
+        var history = window.history;
+        if (!history) {
+            return;
+        }
 
-    // Add a new state so we can intercept the back button
-    history.pushState(null, null, null);
+        if (!history.state) {
+            // Case 1: We came to the page for the first time
 
-    window.onpopstate = function (event) {
-        if (history.state) {
-            console.log(history.length, history.state.historyLength);
-            history.back();
+            console.log("Setting up initial state");
+            var historyLength = history.length;
+            // Set the state to have the length plus one
+            history.replaceState({
+                direction: -1,
+                historyLength: historyLength
+            }, null, null);
+
+            // Add a new state so we can intercept the back button
+            history.pushState({
+                direction: 1,
+                historyLength: historyLength
+            }, null, null);
+        }
+
+        var lastState = history.state;
+        console.log("lastState: ", lastState);
+
+        console.log("Set up state handling");
+        window.onpopstate = function (event) {
+            console.log("pop state: ", event);
+            if (history.state.direction &&
+                history.state.direction !== lastState.direction &&
+                history.state.direction === -1
+                ) {
+                console.log("inside if");
+                var direction = history.state.direction;
+                var action = direction > 0 ? onforward : onback;
+                if (action && typeof (action) === "function") {
+                    action(history.length - history.state.historyLength, function () {
+                        history.go(direction);
+                    });
+                } else {
+                    history.go(history.state.direction);
+                }
+            }
+            lastState = history.state;
+        };
+
+        if (lastState && lastState.direction === -1) {
+            // Go forward
+            console.log("Forwards to get to last one");
+            history.forward();
         }
     };
-})();
+
+    window.addEventListener("pageshow", function () {
+        console.log("This happened");
+        setup();
+    });
+})(window, window.onback, window.onforward);
