cd2923e40aa0981ef8f74332769f86e132349da0
Tom Jacques <tejacques@gmail.com>
2014-12-17 18:58:25 -0500

    Clean up backtracker.js, add dist folder, and swap order of scripts in example to test that order doesn't matter.

--- src/backtracker.js
+++ src/backtracker.js
@@ -1,39 +1,38 @@
-(function (window, onback) {
+(function (window) {
+    // Must have HTML5 history API
+    var history = window.history;
+    if (!history) {
+        return;
+    }
 
     var setup = function () {
-        // Must have HTML5 history API
-        var history = window.history;
-        if (!history) {
-            return;
-        }
-
         if (!history.state) {
             // Case 1: We came to the page for the first time
 
-            var historyLength = history.length;
+            var length = history.length;
 
             // Set the current state to have the current length
             history.replaceState({
                 direction: -1,
-                historyLength: historyLength
+                length: length
             }, null, null);
 
             // Add a new state so we can intercept the back button
             history.pushState({
-                direction: 1,
-                historyLength: historyLength
+                direction: 1
             }, null, null);
         }
 
         var lastState = history.state;
         window.onpopstate = function (event) {
-            if (history.state.direction &&
-                history.state.direction !== lastState.direction &&
-                history.state.direction === -1
+            var state = history.state;
+            var direction = state ? state.direction : 0;
+            if (direction === -1 &&
+                direction !== lastState.direction
                 ) {
-                var direction = history.state.direction;
+                var onback = window.onback;
                 if (onback && typeof (onback) === "function") {
-                    onback(history.length - history.state.historyLength, function () {
+                    onback(history.length - state.length, function () {
                         history.go(direction);
                     });
                 } else {
@@ -49,7 +48,6 @@
         }
     };
 
-    window.addEventListener("pageshow", function () {
-        setup();
-    });
-})(window, window.onback);
+    window.addEventListener("pageshow", setup);
+
+})(window);
